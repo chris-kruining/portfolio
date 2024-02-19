c1ac59c3a17e20646359b93bfe8ae38c6a5f22c6
Chris Kruining <chris.kruining@archipel.academy>
2024-02-19 08:06:21 +0100

    add base path for images

--- solid-start/src/entry-server.tsx
+++ solid-start/src/entry-server.tsx
@@ -6,6 +6,7 @@ export default createHandler(() => (
       return (
       <html lang="en">
         <head>
+          <base href="/portfolio" />
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
