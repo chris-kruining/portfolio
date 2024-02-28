cb6e11889ceeb81b7a6f41561872e6e4fa3e52c2
Chris Kruining <chris.kruining@archipel.academy>
2024-02-28 12:30:28 +0100

    UI tweaks for search

--- solid-start/src/feature/search/search.module.css.d.ts
+++ solid-start/src/feature/search/search.module.css.d.ts
@@ -1,1 +1,2 @@
 export const host: string;
+export const form: string;
--- solid-start/src/feature/search/search.tsx
+++ solid-start/src/feature/search/search.tsx
@@ -1,6 +1,6 @@
 import { For, Show, createEffect, onMount } from 'solid-js';
 import { useSearch } from './search.context';
-import { host } from './search.module.css';
+import { form, host } from './search.module.css';
 
 export function Search() {
     const { results, searchAction } = useSearch();
@@ -15,22 +15,29 @@ export function Search() {
         }
     });
 
-    return <search class={host} popover="auto" ref={resultsContainer}>
-        <form action={searchAction} method="post" role="search">
+    return <>
+        <form class={form} action={searchAction} method="post" role="search">
             <input type="search" name="query" placeholder="Search for ..." value="bl" />
             <button type="submit">Go!</button>
         </form>
 
-        <main>
-            <Show when={results().length > 0}>
-                <span>{results().length} results</span>
+        <search class={host} popover="auto" ref={resultsContainer}>
+            <form action={searchAction} method="post" role="search">
+                <input type="search" name="query" placeholder="Search for ..." value="bl" />
+                <button type="submit">Go!</button>
+            </form>
 
-                <ul>
-                    <For each={results()}>
-                        {result => <li><a href={result.url}>{result.label}</a></li>}
-                    </For>
-                </ul>
-            </Show>
-        </main>
-    </search>
+            <main>
+                <Show when={results().length > 0}>
+                    <span>{results().length} results</span>
+
+                    <ul>
+                        <For each={results()}>
+                            {result => <li><a href={result.url}>{result.label}</a></li>}
+                        </For>
+                    </ul>
+                </Show>
+            </main>
+        </search>
+    </>
 }
